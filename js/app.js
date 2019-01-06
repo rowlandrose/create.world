b6416b80c73bc0b670667863ea596e55276654bb
Rowland Rose <rowlandrose@gmail.com>
2019-01-06 14:41:57 -0500

    Customized away from teamtickets and to create.world

--- js/app.js
+++ js/app.js
@@ -23,8 +23,8 @@ new Vue({
     <div>
         <nav class="navbar" role="navigation" aria-label="main navigation">
             <div class="navbar-brand">
-                <router-link to="/" class="navbar-item">
-                    <img class="nav-logo" src="/img/logo.png"> Team Tickets
+                <router-link to="/" class="navbar-item is-size-3">
+                    <img class="nav-logo" src="/img/logo.png"> Create.World
                 </router-link>
 
                 <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
@@ -36,50 +36,9 @@ new Vue({
 
             <div id="navbarBasicExample" class="navbar-menu">
                 <div class="navbar-start">
-                    <router-link to="/register" class="navbar-item">
-                        Register
+                    <router-link to="/old_school" class="navbar-item">
+                        Old School RPG Map Generator
                     </router-link>
-
-                    <router-link to="/tickets" class="navbar-item">
-                        Tickets {{ this.$store.state.count }}
-                    </router-link>
-
-                    <a class="navbar-item" @click="buttonClick">Increment</a>
-
-                    <div class="navbar-item has-dropdown is-hoverable">
-                        <a class="navbar-link">
-                        More
-                        </a>
-
-                        <div class="navbar-dropdown">
-                        <a class="navbar-item">
-                            {{ test_msg }}
-                        </a>
-                        <a class="navbar-item">
-                            Jobs
-                        </a>
-                        <a class="navbar-item">
-                            Contact
-                        </a>
-                        <hr class="navbar-divider">
-                        <a class="navbar-item">
-                            Report an issue
-                        </a>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="navbar-end">
-                    <div class="navbar-item">
-                        <div class="buttons">
-                            <router-link to="/register" class="button is-primary">
-                                <strong>Register</strong>
-                            </router-link>
-                            <router-link to="/sign_in" class="button is-light">
-                                Sign in
-                            </router-link>
-                        </div>
-                    </div>
                 </div>
             </div>
         </nav>
@@ -87,7 +46,7 @@ new Vue({
         <footer class="footer">
             <div class="content has-text-centered">
                 <p>
-                <strong>Team Tickets</strong> by <a href="http://www.rowlandrose.com">Rowland Rose</a> &mdash; &copy;2018
+                <strong>Create.World</strong> by <a href="http://www.rowlandrose.com">Rowland Rose</a> &mdash; &copy;2019
                 </p>
             </div>
         </footer>
--- js/router.js
+++ js/router.js
@@ -3,13 +3,6 @@ var router = new VueRouter({
     mode: 'history',
     routes: [
         { path: '/', component: Home },
-        { path: '/tickets', component: Tickets },
-        { path: '/register', component: Register },
-        { path: '/email_verify', component: EmailVerify },
-        { path: '/email_verify/:token', component: EmailVerify },
-        { path: '/sign_in', component: SignIn },
-        { path: '/teams', component: Teams },
-        { path: '/edit_team', component: EditTeam },
-        { path: '/edit_team/:team_id', component: EditTeam }
+        { path: '/old_school', component: OldSchool },
     ]
 });
